76f162fbb80af28661b4718b8d810527ee3bfd6f
Victor Parente <ovictorparente@outlook.com>
2024-03-25 18:15:48 -0300

    Introdução de funções e import

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-print("""
+import os
 
+def exibir_nome_do_programa():
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -8,10 +10,34 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
       """)
 
-print("1. Cadastrar Restaurante")
-print("2. Lista Restaurante")
-print("3. Ativar Restaurante")
-print("4. Sair\n")
+def exibir_opcoes():
+    print("1. Cadastrar Restaurante")
+    print("2. Listar Restaurante")
+    print("3. Ativar Restaurante")
+    print("4. Sair\n")
 
-opcao_escolhida = input("Escolha uma opção: ")
-print(f"Você escolheu a opção {opcao_escolhida}")
+def finalizar_app():
+    os.system("cls") #limpar terminal
+    print("Finalizando o app")
+
+def escolher_opcoes():
+    opcao_escolhida = int(input("Escolha uma opção: "))
+    match opcao_escolhida:
+        case 1:
+            print("Cadastrar Restaurante")
+        case 2:
+            print("Listar Restaurantes")
+        case 3:
+            print("Ativar Restaurante")
+        case 4:
+            finalizar_app()
+        case _:
+            print("Opção Inválida!")
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ == "__main__":
+    main()
